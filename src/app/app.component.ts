c89dabf101cb0ab96ea69b3be90ad02382038389
Marco <edrson2@gmail.com>
2023-10-11 22:58:08 -0600

    subir1

---
+++ src/app/app.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { FormGroup, FormBuilder,  Validators } from '@angular/forms';
+
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  myForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.myForm = this.fb.group({
+      dpi: ['', [Validators.required, Validators.pattern(/^[0-9]{4}\s?[0-9]{5}\s?[0-9]{4}$/)]],
+      email: ['', [Validators.required, Validators.pattern(/[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/)]],
+      phone: ['', [Validators.required, Validators.pattern(/^502\s\d{8}$/)]]
+    });
+  }
+}
