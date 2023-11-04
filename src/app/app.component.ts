379bb841cfa75a280a4b20fe2cd44a526a388a54
Ederson Yol <edrson2@gmail.com>
2023-11-04 08:18:10 -0600

    Examen

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,9 +12,11 @@ export class AppComponent {
 
   constructor(private fb: FormBuilder) {
     this.myForm = this.fb.group({
-      dpi: ['', [Validators.required, Validators.pattern(/^[0-9]{4}\s?[0-9]{5}\s?[0-9]{4}$/)]],
-      email: ['', [Validators.required, Validators.pattern(/[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/)]],
-      phone: ['', [Validators.required, Validators.pattern(/^502\s\d{8}$/)]]
+      // dpi: ['', [Validators.required, Validators.pattern(/^[0-9]{4}\s?[0-9]{5}\s?[0-9]{4}$/)]],
+      // email: ['', [Validators.required, Validators.pattern(/[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/)]],
+      // phone: ['', [Validators.required, Validators.pattern(/^502\s\d{8}$/)]],
+      expresion1: ['', [Validators.required, Validators.pattern(/^[A-Ja-j]-\d{8}$/)]],
+      expresion2: ['', [Validators.required, Validators.pattern(/^[2-9][0-9]{2}-[0-9]{3}-[0-9]{4}$/)]]
     });
   }
 }
